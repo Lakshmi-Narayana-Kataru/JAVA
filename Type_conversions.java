4260843bf3d1ec61c8dc50f5b733008b809960cc
Lakshmi-Narayana-Kataru <lakshman.k2934@gmail.com>
2024-10-15 20:23:54 +0530

    Add files via upload
    Type_convertions

---
+++ Type_conversions.java
@@ -0,0 +1,19 @@
+package com.learning.java_basics;
+public class Type_conversions {
+
+	public static void main(String[] args)
+	{
+
+		double d = 100.04;
+
+		long l = (long)d;
+
+		int i = (int)l;
+
+		System.out.println("Double value " + d);
+
+		System.out.println("Long value " + l);
+
+		System.out.println("Int value " + i);
+	}
+}
