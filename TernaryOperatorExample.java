ab1de860c87718891c41eaa9e3324f8452d0a716
Lakshmi-Narayana-Kataru <lakshman.k2934@gmail.com>
2024-10-15 19:55:17 +0530

    TernaryOperatorExample in Java
    Day_08_

---
+++ TernaryOperatorExample.java
@@ -0,0 +1,12 @@
+package com.learning.java_basics;
+
+public class TernaryOperatorExample {
+   // main function
+ public static void main(String[] args)
+ {
+     boolean condition = true;
+     String result = (condition) ? "True" : "False";
+   
+     System.out.println(result);
+ }
+}
