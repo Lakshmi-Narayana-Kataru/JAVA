e11a0b48143034cabe21481f49c6df2ac86e2020
Lakshmi-Narayana-Kataru <lakshman.k2934@gmail.com>
2024-10-15 19:39:57 +0530

    Relational_Oparators in java

---
+++ RelationalOperators.java
@@ -0,0 +1,20 @@
+package com.learning.java_basics;
+
+import java.util.Scanner;
+
+public class RelationalOperators {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+	int num1 =1;
+	int num2 = 2;
+	
+	
+	System.out.println("num1 > num2 is " + (num1 > num2));
+	System.out.println("num1 < num2 is " + (num1 < num2));
+	System.out.println("num1 >= num2 is " + (num1 >= num2));
+	System.out.println("num1 <= num2 is " + (num1 <= num2));
+	System.out.println("num1 == num2 is " + (num1 == num2));
+	System.out.println("num1 != num2 is " + (num1 != num2));
+}
+}
